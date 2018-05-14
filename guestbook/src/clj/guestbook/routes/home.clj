8472a2c3238b6d6ce787aa68b96e314422c772bf
JT W <jwolohan@gmail.com>
2018-05-14 10:17:26 -0400

    Added submit messages to templates and routing

--- guestbook/src/clj/guestbook/routes/home.clj
+++ guestbook/src/clj/guestbook/routes/home.clj
@@ -1,18 +1,48 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
             [guestbook.db.core :as db]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [guestbook.db.core :as db]
+            [ring.util.response :refer [redirect]]
+            [struct.core :as st]
+            ))
 
-(defn home-page []
+(defn home-page [{:keys [flash]}]
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
-
+    "home.html"
+    (merge {:messages (db/get-messages)}
+           (select-keys flash [:name :message :errors]))))
+   
 (defn about-page []
   (layout/render "about.html"))
 
+(def message_schema
+  [[:name
+    st/required
+    st/string]
+   [:message
+    st/required
+    st/string
+    {:message "Message must contain at least 10 characters"
+     :validate #(>= (count %) 10)}]])
+
+(defn validate-message [params]
+  (first (st/validate params message-schema)))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+  (do
+    (db/save mesage!
+      (assoc params :timestamp (java.util.Date.)))
+    (response/found "/"))))
+
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
+  ; Note: overloaded route handling--v. cool!
+  (POST "/" request (save-message! request))
   (GET "/about" [] (about-page)))
 
